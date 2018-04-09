0ffbbd30f9264e08f825385f787bd166dcdfc498
James Hughes <jswhughes@gmail.com>
2018-04-09 11:58:40 +0100

    Actually removed gBlock

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "incendium";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff00018087a70e029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101dcafc31104b7156dd03d6817693d18867bdba2789fb6d09b5d05f559c046398f";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
